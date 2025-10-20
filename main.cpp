0201ae718f85b4cd5fc8cff49383e3cfb9c31278
DavidIbarra01 <2430249@upv.edu.mx>
2025-10-20 17:46:56 -0600

    Agregando metodos Genericos

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
+template <typename T>
+class MatrizBase {
+protected:
+    int _filas;
+    int _columnas;
+
+public:
+    virtual void cargarValores() = 0;
+    virtual MatrizBase<T>* sumar(const MatrizBase<T>& otra) const = 0;
+    virtual void imprimir() const = 0;
+};
+
+template <typename T>
+class MatrizDinamica : public MatrizBase<T> {
+    T **_datos;
+};
+
+template <typename T, int M, int N>
+class MatrizEstatica : public MatrizBase<T> {
+    T _datos[M][N];
+};
+
+
 int main() {
+
     return 0;
 }
